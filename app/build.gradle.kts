7e88ac3c608a396080bd9fdb13b55b630c835d32
Yers <nadyrcoop@gmail.com>
2026-05-26 12:08:41 +0500

    updated versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "kz.yers.quiz"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
